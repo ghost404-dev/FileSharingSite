0cf7a98fecbda27b5791f04a724324d012f1f57a
Ghost <trueface0rmrf0@gmail.com>
2025-01-09 05:21:56 +0600

    Добавил в админку отслеживание классов File и User

--- FileSharing/api/admin.py
+++ FileSharing/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, File
 
-# Register your models here.
+@admin.register(User)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('username','password','email') 
+    list_filter = ('username',)
+
+@admin.register(File)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('user','filename','file_type') 
+    list_filter = ('user',)
