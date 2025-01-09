6b6e722b44b37af63d43cfce36201474da1cbef5
Ghost <trueface0rmrf0@gmail.com>
2025-01-09 19:03:01 +0600

    Добавил функционал для удаления и скачивания файлов через API
    - Реализовал эндпоинт для удаления файлов по ID (DELETE /api/files/<file_id>/delete/).
    - Реализовал эндпоинт для скачивания файлов по ID (GET /api/files/<file_id>/download/).
    - Добавлена проверка, чтобы файлы можно было удалять и скачивать только владельцам файлов.
    - Обработаны исключения для несуществующих файлов или файлов, не принадлежащих текущему пользователю.

--- FileSharing/api/urls.py
+++ FileSharing/api/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import FileViewSet, UserRegisterView
+from .views import FileViewSet, UserRegisterView, FileDeleteView, FileDownloadView
 from django.urls import path
 
 router = DefaultRouter()
@@ -7,6 +7,8 @@ router.register(r'files', FileViewSet, basename='file')
 
 urlpatterns = router.urls
 
-urlpatterns = [
+urlpatterns += [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('files/<int:file_id>/delete/', FileDeleteView.as_view(), name='file-delete'),
+    path('files/<int:file_id>/download/', FileDownloadView.as_view(), name='file-download'),
 ]
--- FileSharing/api/views.py
+++ FileSharing/api/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
+from django.http import FileResponse
 from rest_framework.views import APIView
 from .models import File
+from django.conf import settings
 from .serializers import FileSerializer, UserRegisterSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class FileViewSet(viewsets.ModelViewSet):
@@ -18,6 +21,29 @@ class FileViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         # Устанавливаем текушего пользователя при создании файла
         serializer.save(user=self.request.user)
+
+class FileDeleteView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def delete(self, request, file_id):
+        try:
+            file = File.objects.get(id=file_id, user=request.user)
+            file.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except file.DoesNotExist:
+            return Response({"detail: Файл не найден или вы не владелец файла."},status=status.HTTP_404_NOT_FOUND)
+
+class FileDownloadView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, file_id):
+        try:
+            file = File.objects.get(id=file_id, user=request.user)
+            file_path = file.file.path  # Путь до файла в файловой системе
+            response = FileResponse(open(file_path, 'rb'))
+            return response
+        except File.DoesNotExist:
+            return Response({"detail": "Файл не найден или вы не владелец файла."}, status=status.HTTP_404_NOT_FOUND)
 
 class UserRegisterView(APIView):
     def  post(self, request, *args, **kwargs):
